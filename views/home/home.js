d6c4945a5ff1e78a91cc8c38a1a3bb255ebe025a
Juan Bonforti <juan@synapticlinks.com.ar>
2019-10-26 10:19:28 -0300

    Improves on project structure

--- views/home/home.js
+++ views/home/home.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 
-
 export default function Home() {
   return (
     <View style={styles.container}>
